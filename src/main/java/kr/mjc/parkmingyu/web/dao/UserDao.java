07c09b9d9decc530cf43bd2bcdc56556a6d36305
min gyu <pmk7182@naver.com>
2021-05-30 22:17:33 +0900

    web

--- src/main/java/kr/mjc/parkmingyu/web/dao/UserDao.java
+++ src/main/java/kr/mjc/parkmingyu/web/dao/UserDao.java
@@ -1,6 +1,8 @@
 package kr.mjc.parkmingyu.web.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -15,23 +17,28 @@ import java.util.Map;
 @Component
 public class UserDao {
 
-  private static final String LIST_USERS =
-      "select userId, email, name from user order by userId desc limit ?,?";
+  private static final String LIST_USERS
+          = "select userId, email, name from user order by userId desc limit ?,?";
 
-  private static final String ADD_USER =
-      "insert user(email, password, name) values(:email, sha2(:password,256), :name)";
+  public static final String COUNT_USERS = "select count(userId) from user";
 
-  private static final String LOGIN =
-      "select userId, email, name from user where (email, password) = (?, sha2(?,256))";
+  private static final String ADD_USER = """
+      insert user(email, password, name) 
+      values(:email, sha2(:password,256), :name)""";
+
+  private static final String LOGIN = """
+      select userId, email, name from user 
+      where (email, password) = (?, sha2(?,256))""";
 
   private static final String GET_USER =
-      "select userId, email, name from user where userId=?";
+          "select userId, email, name from user where userId=?";
 
-  private static final String UPDATE_EMAIL =
-      "update user set email=:email where userId=:userId";
+  public static final String UPDATE_USER =
+          "update user set email=:email, name=:name where userId=:userId";
 
-  private static final String UPDATE_PASSWORD =
-      "update user set password=sha2(:newPassword,256) where userId=:userId and password=sha2(:password,256)";
+  private static final String UPDATE_PASSWORD = """
+      update user set password=sha2(:newPassword,256)
+      where userId=:userId and password=sha2(:password,256)""";
 
   private JdbcTemplate jdbcTemplate;
 
@@ -46,30 +53,66 @@ public class UserDao {
     this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
   }
 
+  /**
+   * 사용자 목록
+   */
   public List<User> listUsers(int offset, int count) {
     return jdbcTemplate.query(LIST_USERS, rowMapper, offset, count);
   }
 
-  public User login(String email, String password) {
+  /**
+   * 사용자 수
+   */
+  public int countUsers() {
+    return jdbcTemplate.queryForObject(COUNT_USERS, Integer.class);
+  }
+
+  /**
+   * 로그인
+   *
+   * @return 로그인한 사용자 정보
+   * @throws EmptyResultDataAccessException 로그인에 실패할 경우
+   */
+  public User login(String email, String password)
+          throws EmptyResultDataAccessException {
     return jdbcTemplate.queryForObject(LOGIN, rowMapper, email, password);
   }
 
+  /**
+   * 사용자 조회
+   */
   public User getUser(int userId) {
     return jdbcTemplate.queryForObject(GET_USER, rowMapper, userId);
   }
 
-  public void addUser(User user) {
+  /**
+   * 사용자 등록
+   *
+   * @throws DuplicateKeyException 이메일이 중복되어 사용자 등록에 실패할 경우
+   */
+  public void addUser(User user) throws DuplicateKeyException {
     namedParameterJdbcTemplate
-        .update(ADD_USER, new BeanPropertySqlParameterSource(user));
+            .update(ADD_USER, new BeanPropertySqlParameterSource(user));
   }
 
-  public int updateEmail(int userId, String email) {
-    Map<String, Object> params = new HashMap<>();
-    params.put("userId", userId);
-    params.put("email", email);
-    return namedParameterJdbcTemplate.update(UPDATE_EMAIL, params);
+  /**
+   * 이메일 수정
+   *
+   * @throws DuplicateKeyException 이메일이 중복되어 이메일 수정에 실패할 경우
+   */
+  public void updateUser(User user) throws DuplicateKeyException {
+    namedParameterJdbcTemplate
+            .update(UPDATE_USER, new BeanPropertySqlParameterSource(user));
   }
 
+  /**
+   * 비밀번호 수정
+   *
+   * @param userId      사용자 아이디
+   * @param password    현재 비밀번호
+   * @param newPassword 새 비밀번호
+   * @return 수정한 행의 갯수. 0은 아이디와 비밀번호가 틀려 수정을 못한 경우
+   */
   public int updatePassword(int userId, String password, String newPassword) {
     Map<String, Object> params = new HashMap<>();
     params.put("userId", userId);
--- src/main/java/kr/mjc/parkmingyu/web/springmvc/v2/UserController.java
+++ src/main/java/kr/mjc/parkmingyu/web/springmvc/v2/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.List;
 
 /**
@@ -32,12 +34,23 @@ public class UserController {
    */
   @GetMapping("/userList")
   public void userList(
-      @RequestParam(required = false, defaultValue = "1") int page,
-      Model model) {
-    int count = 25;
+          @RequestParam(required = false, defaultValue = "1") int page,
+          @RequestParam(required = false, defaultValue = "20") int count,
+          Model model) {
     int offset = (page - 1) * count;
     List<User> userList = userDao.listUsers(offset, count);
+    int totalCount = userDao.countUsers();
     model.addAttribute("userList", userList);
+    model.addAttribute("totalCount", totalCount);
+  }
+
+  /**
+   * 사용자 정보 화면
+   */
+  @GetMapping("/userInfo")
+  public void userInfo(@RequestParam(required = false) Integer userId,
+                       Model model) {
+    model.addAttribute("user", userDao.getUser(userId));
   }
 
   /**
@@ -62,14 +75,54 @@ public class UserController {
    */
   @PostMapping("/login")
   public String login(@RequestParam String email, @RequestParam String password,
+                      @RequestParam(required = false, defaultValue = "/") String returnUrl,
                       HttpSession session, RedirectAttributes attributes) {
     try {
       User user = userDao.login(email, password);
       session.setAttribute("USER", user);
-      return "redirect:/springmvc/v2/user/userInfo";
-    } catch (EmptyResultDataAccessException e) {
+      return "redirect:" + returnUrl; // 로그인 성공할 경우 returnUrl로
+    } catch (EmptyResultDataAccessException e) { // 로그인 실패할 경우
+      attributes.addFlashAttribute("email", email);
       attributes.addFlashAttribute("msg", "Wrong email or password");
-      return "redirect:/springmvc/v2/user/loginForm";
+      return "redirect:/springmvc/v2/user/loginForm?returnUrl=" +
+              URLEncoder.encode(returnUrl, Charset.defaultCharset());
+    }
+  }
+
+  /**
+   * 개인정보 수정 액션
+   */
+  @PostMapping("/updateUser")
+  public String updateUser(User user,
+                           @SessionAttribute("USER") User sessionUser,
+                           RedirectAttributes attributes) {
+    try {
+      user.setUserId(sessionUser.getUserId());
+      userDao.updateUser(user);
+      // 개인정보 수정 후에 세션 업데이트
+      sessionUser.setEmail(user.getEmail());
+      sessionUser.setName(user.getName());
+      return "redirect:/springmvc/v2/user/myInfo";
+    } catch (DuplicateKeyException e) {
+      attributes.addFlashAttribute("user", user);
+      attributes.addFlashAttribute("msg", "Duplicate email");
+      return "redirect:/springmvc/v2/user/userEdit";
+    }
+  }
+
+  /**
+   * 비밀번호 수정 액션
+   */
+  @PostMapping("/updatePassword")
+  public String updatePassword(String password, String newPassword,
+                               @SessionAttribute("USER") User user, RedirectAttributes attributes) {
+    int result =
+            userDao.updatePassword(user.getUserId(), password, newPassword);
+    if (result > 0) {
+      return "redirect:/springmvc/v2/user/myInfo";
+    } else {
+      attributes.addFlashAttribute("msg", "Wrong password");
+      return "redirect:/springmvc/v2/user/passwordForm";
     }
   }
 
